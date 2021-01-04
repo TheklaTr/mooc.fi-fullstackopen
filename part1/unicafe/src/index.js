e89a017bcbed6b534d9bdcee63ff3cc942a0451d
TrHien <ttthuhien0104@gmail.com>
2021-01-04 17:32:51 +0200

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,6 +8,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = (good / all) * 100 + ' %';
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -18,6 +22,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   );
 };
