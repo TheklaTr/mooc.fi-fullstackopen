1da019b9309bdd55a050ea9d96174bc7f760ac41
TrHien <ttthuhien0104@gmail.com>
2021-01-06 21:20:53 +0200

    2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
+import Filter from './components/Filter';
+import PersonForm from './components/PersonForm';
+
 const App = () => {
    const [persons, setPersons] = useState([
       { name: 'Arto Hellas', number: '040-123456' },
-      { name: 'Ada Lovelace', number: '39-44-5323523' },
-      { name: 'Dan Abramov', number: '12-43-234345' },
-      { name: 'Mary Poppendieck', number: '39-23-6423122' },
    ]);
 
    const [newName, setNewName] = useState('');
@@ -50,22 +50,15 @@ const App = () => {
    return (
       <div>
          <h2>Phonebook</h2>
-         <div>
-            filter shown with
-            <input value={filterPerson} onChange={handleFilterChange} />
-         </div>
+         <Filter filter={filterPerson} handleChange={handleFilterChange} />
          <h2>add a new</h2>
-         <form onSubmit={addPerson}>
-            <div>
-               name: <input value={newName} onChange={handleNameChange} />
-            </div>
-            <div>
-               number: <input value={newNumber} onChange={handleNumberChange} />
-            </div>
-            <div>
-               <button type="submit">add</button>
-            </div>
-         </form>
+         <PersonForm
+            handleSubmit={addPerson}
+            name={newName}
+            nameOnChange={handleNameChange}
+            number={newNumber}
+            numberOnChange={handleNumberChange}
+         />
          <h2>Numbers</h2>
          {personsToShow.map((person) => (
             <p key={person.name}>
---
+++ part2/phonebook/src/components/PersonForm.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const PersonForm = (props) => {
+   return (
+      <form onSubmit={props.handleSubmit}>
+         <div>
+            name: <input value={props.name} onChange={props.nameOnChange} />
+         </div>
+         <div>
+            number:{' '}
+            <input value={props.number} onChange={props.numberOnChange} />
+         </div>
+         <div>
+            <button type="submit">add</button>
+         </div>
+      </form>
+   );
+};
+
+export default PersonForm;
