f512ca5243a27cafbf755f5a1f7a0ce9d4abc698
TrHien <ttthuhien0104@gmail.com>
2021-08-02 00:23:05 +0300

    4.10: Blog list tests, step3

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,15 +2,21 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs.map((blog) => blog.toJSON()))
+   const blogs = await Blog.find({})
+   response.json(blogs.map((blog) => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+   const body = request.body
+   const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      URL: body.URL,
+      likes: body.likes,
+   })
 
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+   const savedBlog = await blog.save()
+   response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -32,6 +32,30 @@ test('the unique identifier property of the blog posts', async () => {
    expect(response.body[0].id).toBeDefined()
 })
 
+test('successfully creates a new blog post ', async () => {
+   const newBlog = {
+      title: 'test add blog',
+      author: 'Author Test 1',
+      url: 'https://testblog1.com',
+      likes: 0,
+   }
+
+   const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+   const blogsAtEnd = await helper.blogsInDb()
+
+   // verify that the total number of blogs in the system is increased by one
+   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+   // verify that the content of the blog post is saved correctly to the database
+   const titles = blogsAtEnd.map((blog) => blog.title)
+   expect(titles).toContain('test add blog')
+})
+
 afterAll(() => {
    mongoose.connection.close()
 })
