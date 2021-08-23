f994e75cbc986a73d38688e1ee501e7a393d8291
TrHien <ttthuhien0104@gmail.com>
2021-08-23 13:38:16 +0300

    4.23*: bloglist expansion, step11

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+let token = ''
 
 beforeEach(async () => {
    await Blog.deleteMany({})
@@ -16,11 +20,29 @@ beforeEach(async () => {
    }
 })
 
+beforeAll(async (done) => {
+   await User.deleteMany({})
+
+   const passwordHash = await bcrypt.hash('testing', 10)
+   const user = new User({ username: 'root', passwordHash })
+
+   await user.save()
+
+   await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'testing' })
+      .end((err, res) => {
+         token = res.body.token
+         done()
+      })
+})
+
 test('the amount of blogs returned is correct', async () => {
    const blogsAtStart = await helper.blogsInDb()
 
    const response = await api
       .get('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
@@ -28,7 +50,10 @@ test('the amount of blogs returned is correct', async () => {
 })
 
 test('the unique identifier property of the blog posts', async () => {
-   const response = await api.get('/api/blogs')
+   const response = await api
+      .get('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
+
    expect(response.body[0].id).toBeDefined()
 })
 
@@ -42,6 +67,7 @@ test('successfully creates a new blog post ', async () => {
 
    await api
       .post('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
@@ -56,6 +82,20 @@ test('successfully creates a new blog post ', async () => {
    expect(titles).toContain('test add blog')
 })
 
+test('fails with status 401 if unauthorized', async () => {
+   const newBlog = {
+      title: 'test add blog',
+      author: 'Author Test 2',
+      likes: 5,
+   }
+
+   await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+})
+
 test("the 'likes' property will default to 0 when missing form the request", async () => {
    const newBlog = {
       title: 'test add blog without likes',
@@ -63,7 +103,10 @@ test("the 'likes' property will default to 0 when missing form the request", asy
       url: 'https://testblog2.com',
    }
 
-   const savedBlog = await api.post('/api/blogs').send(newBlog)
+   const savedBlog = await api
+      .post('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
+      .send(newBlog)
 
    expect(savedBlog.body.likes).toBe(0)
 })
@@ -73,7 +116,11 @@ test('a blog without title and url', async () => {
       author: 'Author Test 3',
    }
 
-   await api.post('/api/blogs').send(newBlog).expect(400)
+   await api
+      .post('/api/blogs')
+      .set('Authorization', `bearer ${token}`)
+      .send(newBlog)
+      .expect(400)
 
    const blogsAtEnd = await helper.blogsInDb()
 
@@ -84,7 +131,10 @@ test('succeeds with status code 204 if id is valid', async () => {
    const blogsAtStart = await helper.blogsInDb()
    const blogToDelete = blogsAtStart[0]
 
-   await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+   await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `bearer ${token}`)
+      .expect(204)
 
    const blogsAtEnd = await helper.blogsInDb()
    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
@@ -107,6 +157,7 @@ test('succeed updating a blog with valid id and info', async () => {
 
    await api
       .put(`/api/blogs/${blogToUpdate.id}`)
+      .set('Authorization', `bearer ${token}`)
       .send(updatedBlog)
       .expect(200)
       .expect('Content-Type', /application\/json/)
