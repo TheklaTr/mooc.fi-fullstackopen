fbb37099c59239d12ae0a3c9514b00268762e70e
TrHien <ttthuhien0104@gmail.com>
2021-09-27 14:30:42 +0300

    8.2: All books

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -86,9 +86,16 @@ let books = [
 ]
 
 const typeDefs = gql`
+   type Book {
+      title: String!
+      author: String!
+      published: String!
+      genres: [String!]!
+   }
    type Query {
       bookCount: Int!
       authorCount: Int!
+      allBooks: [Book!]!
    }
 `
 
@@ -96,6 +103,7 @@ const resolvers = {
    Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
+      allBooks: () => books,
    },
 }
 
